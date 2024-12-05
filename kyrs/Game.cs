28d0068ba0c6374852de79efb26ee9c18cfb3604
asdfkfdjg <153234753+asdfkfdjg@users.noreply.github.com>
2024-12-05 14:36:17 +0700

    Add files via upload

---
+++ kyrs/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace Match3V2
+{
+    public class Game
+    {
+        private readonly Grid _grid;
+
+        public Game(Canvas canvas, int rows, int columns)
+        {
+            _grid = new Grid(canvas, rows, columns, OnTilesMatched, OnTileMoved);
+        }
+
+        public void Start()
+        {
+            _grid.GenerateGrid();
+        }
+
+        private void OnTilesMatched(int count)
+        {
+            // Увеличить счёт на основе удалённых клеток
+        }
+
+        private void OnTileMoved()
+        {
+            // Логика, которая может быть выполнена после перемещения клетки
+        }
+    }
+}
---
+++ kyrs/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Match3V2
+{
+    public partial class MainWindow : Window
+    {
+        private Game _game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            _game = new Game(GameCanvas, 8, 8);
+            _game.Start();
+        }
+    }
+}
